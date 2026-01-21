9aa74342f92ebced1d2bfc66f9495483f98e4d3a
Forsakenftw <leoxzayn@gmail.com>
2026-01-21 10:13:47 +0530

    updated auth controller

---
+++ myapp/src/main/java/com/example/myapp/dto/loginrequest.java
@@ -0,0 +1,9 @@
+package com.example.myapp.dto;
+
+import lombok.Data;
+
+@Data
+public class loginrequest {
+    private String email;
+    private String password;
+}
--- myapp/src/main/java/com/example/myapp/model/User.java
+++ myapp/src/main/java/com/example/myapp/model/User.java
@@ -1,26 +1,43 @@
 package com.example.myapp.model;
 
-import jakarta.annotation.Generated;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Data;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "users")
-@Data
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
-    private String name;
+
     @Column(nullable = false, unique = true)
     private String email;
+
     @Column(nullable = false)
     private String password;
 
+    // getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
